42f4328630e1ea1bc8c9b7d549a33cd3bc73b375
Adityakataria18 <79639669+Adityakataria18@users.noreply.github.com>
2021-06-01 17:44:20 +0530

    Update snow.js

--- snow.js
+++ snow.js
@@ -5,7 +5,7 @@ class Snow{
           density:2 
        }
        this.body(x,-10,50,50)
-       this.body.image("snow4.webp")
+       this.image=loadImage("snow4.webp")
    }
    display(){
       
